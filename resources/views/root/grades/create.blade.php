0c80617c5cd9b179f2b1b7f780286673174593f8
Bryan Eduria <>
2018-09-01 22:49:48 +0800

    Made some changes in UI

--- resources/views/root/grades/create.blade.php
+++ resources/views/root/grades/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     @component('root.components.breadcrumbs')
         @slot('page_title')
-            Create an admin
+            Create grade level
         @endslot
 
         <li class="breadcrumb-item">
